8b7fc952c5cd3fe01734444c94a86da1fdb7fc07
Jacob-13 <jjohn483@uwo.ca>
2024-03-11 13:25:44 -0400

    Slight changes to webcam.py

--- webcam.py
+++ webcam.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
     #the CycleGan takes data as a dictionary
     #easier to work within that constraint than to reright
     # start an infinite loop and keep reading frames from the webcam until we encounter a keyboard interrupt
-    data = {"A": None, "A_paths": None, "B": None, "B_paths": None}
+    data = {"A": None, "A_paths": None, "B": None, "B_paths": None} # B and B_paths are needed for cycle_gan model
     while True:
 
         #ret is bool returned by cap.read() -> whether or not frame was captured succesfully
@@ -85,25 +85,25 @@ if __name__ == '__main__':
         #convert numpy array to tensor
         #need data to be a tensor for compatability with running model. expects floatTensors
         data['A'] = torch.FloatTensor(frame)
-        data['B'] = torch.FloatTensor(frame)
-        
+        #data['B'] = torch.FloatTensor(frame) # Needed for cycle_gan model
         model.set_input(data)  # unpack data from data loader
         model.test()
 
         #get only generated image - indexing dictionary for "fake" key
-        result_image = model.get_current_visuals()['fake_B']
-        #use tensor2im provided by util file
+        result_image = model.get_current_visuals()['fake'] # 'fake_B' for cycle_gan model
+        #use tensor2im function provided by util file
         result_image = util.tensor2im(result_image)
         result_image = cv2.cvtColor(np.array(result_image), cv2.COLOR_BGR2RGB)  
         result_image = cv2.resize(result_image, (512, 512))      
-        result_image = cv2.putText(result_image, str(opt.name)[6:-11], org, font,  # can probably remove
-                   fontScale, color, thickness, cv2.LINE_AA)   
+        #result_image = cv2.putText(result_image, str(opt.name)[6:-11], org, font,  # can probably remove
+        #           fontScale, color, thickness, cv2.LINE_AA)   
         cv2.imshow('style', result_image)
 
         #ASCII value of Esc is 27. Can probably remove the c == 99 one
         c = cv2.waitKey(1)
         if c == 27: # ends on holding esc, maybe we can do something else, like when the window closes or something
             break   # maybe instead of displaying in a window here, can do the react thing, but might not be needed
+        """
         if c == 99:
             if style_model_index == len(style_models): # not needed for us, but to remove error you could add 2 lines that he didn't add found on the webpage
                 style_model_index = 0
@@ -111,7 +111,7 @@ if __name__ == '__main__':
             style_model_index += 1
             model = create_model(opt)      # create a model given opt.model and other options
             model.setup(opt) 
-      
-        
+        """
+
     webcam.release()
     cv2.destroyAllWindows()
